c7a3278b3bb656233e4dabc5eec9238dcfe26654
estherkimyunjung <estherkimyunjung@gmail.com>
2020-07-10 10:00:04 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header'
+import Counter from './Counter'
 
 class App extends Component {
   render() {
@@ -15,44 +16,5 @@ class App extends Component {
   }
 }
 
-class Header extends Component {
-  render() {
-    return (
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
-      </header>
-    );
-  }
-}
-
-class Counter extends Component {
-  state = { count: 0 };
-
-  increment = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-
-  decrement = () => {
-    this.setState({ count: this.state.count - 1 });
-  };
-
-  renderDescription = () => {
-    const remainder = this.state.count % 5;
-    const upToNext = 5 - remainder;
-    return `The current count is less than ${this.state.count + upToNext}`;
-  };
-
-  render() {
-    return (
-      <div className="Counter">
-        <h1>{this.state.count}</h1>
-        <button onClick={this.decrement}> - </button>
-        <button onClick={this.increment}> + </button>
-        <h3>{this.renderDescription()}</h3>
-      </div>
-    );
-  }
-}
 
 ReactDOM.render(<App />, document.getElementById('root'));
